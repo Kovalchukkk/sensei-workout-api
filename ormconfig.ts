08b191bd4cd3c57012d81dc459c8ff0e2e123092
Mykola Kovalchuk <kolyankov@gmail.com>
2024-09-07 22:26:41 +0300

    fix: migration paths

--- ormconfig.ts
+++ ormconfig.ts
@@ -6,10 +6,10 @@ config();
 export const AppDataSource = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: [__dirname + 'dist/**/*.entity.js'],
+  entities: ['dist/**/*.entity.js'],
   logging: true,
   synchronize: false,
   migrationsRun: false,
-  migrations: [__dirname + 'dist/**/migrations/*.js'],
+  migrations: ['dist/**/migrations/*.js'],
   migrationsTableName: 'migrations',
 });
